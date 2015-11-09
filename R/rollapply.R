1913d5ca312da95149071c3cfe8ec394c91cb2c3
fountainer <fountainer@163.com>
2015-11-09 15:56:38 +0800

    add rolling window names

--- R/rollapply.R
+++ R/rollapply.R
@@ -1,4 +1,4 @@
-#' Title
+#' Rolling apply a function on a data frame or matrix.
 #'
 #' @param x A data frame or a matrix.
 #' @param n The rolling windown size.
@@ -18,5 +18,6 @@ roll_apply <- function(x, n, fun, ...) {
     stop("The size of window is larger than row number of the data.")
   }
   vec_seq <- seq_len(n_row - n + 1)
+  names(vec_seq) <- paste0("win", vec_seq)
   return(lapply(vec_seq, function(i) fun(x[i:(i + n - 1), ], ...)))
 }
