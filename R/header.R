4cdf1bc73e244a892066c462dbbeac33fea77610
fountainer <fountainer@163.com>
2015-11-29 00:37:48 +0800

    supper header 6 in make_header

--- R/header.R
+++ R/header.R
@@ -4,7 +4,7 @@
 #'
 #' @param n The number of the first header 3.Default 1.
 #'
-#' @return A function with a integer parameter(3, 4, 5) that can automatically number headers
+#' @return A function with a integer parameter(3, 4, 5, 6) that can automatically number headers
 #' @export
 #'
 #' @examples
@@ -17,6 +17,7 @@ make_header <- function(n = 1) {
   num_hash_3 <- n - 1
   num_hash_4 <- 0L
   num_hash_5 <- 0L
+  num_hash_6 <- 0L
   header <- function(n) {
     if (n == 3) {
       num_hash_3 <<- num_hash_3 + 1
@@ -29,9 +30,13 @@ make_header <- function(n = 1) {
       str_result <- paste(paste0(rep("#", n), collapse = ""), paste0(num_hash_3, ".", num_hash_4), " ")
     } else if (n == 5) {
       num_hash_5 <<- num_hash_5 + 1
+      num_hash_6 <<- 0L
       str_result <- paste(paste0(rep("#", n), collapse = ""), paste0(num_hash_3, ".", num_hash_4, ".", num_hash_5), " ")
+    } else if (n == 6) {
+      num_hash_6 <<- num_hash_6 + 1
+      str_result <- paste(paste0(rep("#", n), collapse = ""), paste0(num_hash_3, ".", num_hash_4, ".", num_hash_5, ".", num_hash_6), " ")
     } else {
-      str_result <- "3 - 5 of # are required."
+      str_result <- "3 - 6 is required."
     }
     return(str_result)
   }
