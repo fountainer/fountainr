3439b9e264ab24ec3d2de0343b74db7639887f65
fountainer <fountainer@163.com>
2016-04-04 15:57:20 +0800

    Using pbapply package to add a progress bar

--- R/rollapply.R
+++ R/rollapply.R
@@ -4,16 +4,18 @@
 #' @param n The rolling windown size.
 #' @param fun A function whose first parameter is the \code{x}.
 #' @param ... Additional parameter passed to \code{fun}
+#' @param progress Create a progress bar("none", "txt", "tk", "win", "timer")
 #' @param SIMPLIFY Attempt to return a data frame if \code{fun} returns a vector or a list whose elements are vectors with length 1.
 #'
 #'
 #' @return A data frame or list containing the rolling results. See \code{SIMPLIFY}
 #' @export
+#' @importFrom pbapply pblapply
 #'
 #' @examples
 #' m <- matrix(1:10, ncol = 2)
 #' roll_apply(m, 4, colMeans)
-roll_apply <- function(x, n, fun, ..., SIMPLIFY = FALSE) {
+roll_apply <- function(x, n, fun, ...,progress = "none", SIMPLIFY = FALSE) {
   fun <- match.fun(fun)
   n_row <- nrow(x)
   if (n_row < n ) {
@@ -21,7 +23,10 @@ roll_apply <- function(x, n, fun, ..., SIMPLIFY = FALSE) {
   }
   vec_seq <- seq_len(n_row - n + 1)
   names(vec_seq) <- paste0("win", vec_seq)
-  res <- lapply(vec_seq, function(i) fun(x[i:(i + n - 1), ], ...))
+
+  op <- pboptions(type = progress)
+
+  res <- pblapply(vec_seq, function(i) fun(x[i:(i + n - 1), ], ...))
   if (SIMPLIFY) {
     if (all(unlist(lapply(res[[1]], is.atomic)) == TRUE) &&
         all(unlist(lapply(res[[1]], function(x) length(x) == 1)) == TRUE)) {
