579e8d0ace9e743c3b1f8b1f63e2ee2d30876f69
fountainer <fountainer@163.com>
2016-06-13 21:43:53 +0800

    shutdown() returns the time when the function is called

--- R/shutdown.R
+++ R/shutdown.R
@@ -2,8 +2,10 @@
 #'
 #' @param times The times to shutdown the computer. Unit is second.
 #' @export
+#' @return The time when the function is called
 #' 
 #' @examples shutdown(10)
 shutdown <- function(times) {
   system(paste("shutdown -s -t", times))
+  return(Sys.time())
 }
